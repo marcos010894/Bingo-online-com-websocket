6810e574f9bcf6e7d2c3d8c4ed79022be6c34434
Marcos Paulo Machado Azevedo <marcospaulo.dev@hotmail.com>
2023-06-29 19:47:43 -0300

    alterando porta

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const WebSocket = require("ws");
 const http = require("http");
 const fs = require("fs");
-
+const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   fs.readFile("index.html", (err, data) => {
@@ -57,4 +57,4 @@ function drawNumber(wss, drawnNumbers) {
   return randomNumber;
 }
 
-server.listen(8000);
+server.listen(port);
