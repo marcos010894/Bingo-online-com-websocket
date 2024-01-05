ea0a88c83bbb77d1e2d5bf0c11b2a0061edd5322
Marcos Paulo Machado Azevedo <marcospaulo.dev@hotmail.com>
2024-01-04 22:13:58 -0300

    ok

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const port = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
+var url = "https://escritorio.invictplanet.com/apibingo/";
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   fs.readFile("index.html", (err, data) => {
@@ -20,10 +21,12 @@ const wss = new WebSocket.Server({ server });
 let started = false;
 var started_client = false;
 let drawnNumbers = [];
+
 var vencedorIs = false;
 var vencedorIs2 = false;
 wss.on("connection", (ws) => {
   ws.on("message", (message) => {
+    console.log(message)
     if (message == "Vencedor1") {
       vencedorIs = true;
     }
@@ -58,6 +61,9 @@ async function drawNumber(wss, drawnNumbers) {
     availableNumbers[Math.floor(Math.random() * availableNumbers.length)];
 
   await drawnNumbers.push(randomNumber);
+  if(drawnNumbers.length > 1){
+    started_client = true;
+  }
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(
@@ -94,4 +100,53 @@ function restartServer() {
     }
     drawNumber(wss, drawnNumbers);
   }, 5000);
+}
+
+var security = true
+if (security == true) {
+    var ws = new WebSocket('wss://bingoinvict-2a0239e0c8b7.herokuapp.com');
+} else {
+    var ws = new WebSocket('ws://localhost:3000');
+}
+
+
+function sendMsg() {
+  ws.send('Olá, servidor!');
+}
+
+var quanty_start = "";
+
+async function fetchDataQuanties() {
+  try {
+    const response = await fetch(url + "verifyQuantitysstart.php");
+    const data = await response.json();
+    quanty_start = data.quantidade_inicio;
+    fetchData();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+
+
+var intervaloParaIniciar = setInterval(function () {
+  fetchDataQuanties()
+}, 5000); // 1000 milissegundos = 1 segundo
+
+
+async function fetchData() {
+  if (started) return;
+  try {
+    const response = await fetch(url + "verifyQuantities.php");
+    const data = await response.json();
+    const faltando = Number(quanty_start) - Number(data.count);
+    if (Number(data.count) >= Number(quanty_start)) {
+      sendMsg()
+      clearInterval(intervaloParaIniciar)
+      console.log('intervalo encerrado')
+
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 }
